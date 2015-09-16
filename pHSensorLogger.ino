07afec8a95b800332309ad1794bdc353fe2c685e
peninquen <jgarciapons@gmail.com>
2015-09-16 23:53:30 +0200

    Update pHSensorLogger.ino

--- pHSensorLogger.ino
+++ pHSensorLogger.ino
@@ -3,9 +3,9 @@
  * An example to collect data from a pH sensor conected to an analog adapter
  *  using the pHSensor class
 
- * version 0.1 ALPHA 15/09/2015
+ * version 0.2 ALPHA 16/09/2015
  * Author: Jaime García  @peninquen
- * Licence: Apache License Version 2.0.
+ * License: Apache License Version 2.0.
  *
  **********************************************************************/
 
@@ -14,23 +14,22 @@
 
 
 #define N_CALPH 2
-#define S_CALPH sizeof(calibrationData);
+#define S_CALPH sizeof(calibration);
 #define EM_CALPH_OFFSET 0
 #define EM_CALPH_END (EM_CALPH_OFFSET + (N_CALPH * S_CALPH))
 #define EM_END       (EM_CALPH_END + 1)
-#define MAGIC_NUMBER 123 // byte number to check valid values in EEPROM
 
 #define REFRESH_INTERVAL  1000   // refresh time, 1 second
 #define WRITE_INTERVAL 5000      // values send to serial port, 5 seconds (5 * 1000)
 #define PH_PIN A0                //
 
-pHSensor pH; // instance to collect data
+pHSensor pH; // instance to process data
 //variables to process and send values
 boolean firstData;
-float pHvalue;
-float pHavg;
-float pHmax;
-float pHmin;
+short pHvalue;
+short pHmax;
+short pHmin;
+long pHavg;
 int counter = 0;
 
 unsigned long previousMillis = 0;
@@ -40,7 +39,7 @@ unsigned long currentMillis = 0;
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  pH.begin(PH_PIN, REFRESH_INTERVAL, EM_CALPH_OFFSET, 25.0);
+  pH.begin(PH_PIN, REFRESH_INTERVAL, EM_CALPH_OFFSET);
   Serial.println("time(s), average pH, max pH, min pH");
 
   firstData = false;
@@ -71,11 +70,11 @@ void loop() {
 
     Serial.print(currentMillis / 1000);
     Serial.print(",");
-    Serial.print(pHvalue);
+    Serial.print((float)pHvalue / 100);
     Serial.print(",");
-    Serial.print(pHmax);
+    Serial.print((float)pHmax / 100);
     Serial.print(",");
-    Serial.println(pHmin);
+    Serial.println((float)pHmin / 100);
 
   }
 }
